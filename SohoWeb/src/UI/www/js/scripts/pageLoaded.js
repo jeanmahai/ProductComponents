23d9b0f34d840347f79fc81c187aecbe817412e3
jean-home <mahai_1986@126.com>
2014-05-11 19:05:40 +0800

    asdfds

---
+++ SohoWeb/src/UI/www/js/scripts/pageLoaded.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jeanma on 14-5-11.
+ */
+(function(){
+
+    function menuHeaderClick(){
+    }
+    function resetContent(){
+    }
+    function pageLoaded(){
+    };
+    $(pageLoaded);
+    $(window).bind("resize",resetContent);
+})();
