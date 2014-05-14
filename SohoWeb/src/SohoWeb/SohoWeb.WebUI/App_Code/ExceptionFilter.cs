399846a12eb5fbed934cda8cc86d66c1ecd4d47c
nick <zhengyanming2006@163.com>
2014-05-14 23:37:29 +0800

    Modify code.

---
+++ SohoWeb/src/SohoWeb/SohoWeb.WebUI/App_Code/ExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace SohoWeb.WebUI
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception != null)
+            {
+                if (AppSettingValue.MobileApiDebug)
+                {
+                    actionExecutedContext.Response = new System.Net.Http.HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(
+                            JsonConvert.SerializeObject(
+                            new PortalResult { Success = false, Message = actionExecutedContext.Exception.Message, Code = 0 }))
+                    };
+                }
+                else
+                {
+                    actionExecutedContext.Response = new System.Net.Http.HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent(
+                            JsonConvert.SerializeObject(
+                            new PortalResult { Success = false, Message = LanguageHelper.GetText("系统错误"), Code = 0 }))
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ SohoWeb/src/SohoWeb/SohoWeb.WebUI/App_Code/PortalResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SohoWeb.WebUI
+{
+    public class PortalResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+        public int Code { get; set; }
+    }
+}
